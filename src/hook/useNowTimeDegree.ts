7bdae0c58ff4e0e17f032ef9b23ad7c5b7230245
7wataaa <65275342+7wataaa@users.noreply.github.com>
2022-09-28 01:28:55 +0900

    [Refactor]  useNowTimeDegreeを修正

--- src/hook/useNowTimeDegree.ts
+++ src/hook/useNowTimeDegree.ts
@@ -1,19 +1,25 @@
 import { useState } from 'react';
 import { useInterval } from 'react-use';
 
+/**
+ * 現在時刻から24時間式アナログ時計の角度を返す
+ * @param {Date} [date=now] 現在時刻を使わない場合の時刻
+ * @return {number} 針の角度
+ */
+export const calcTimeDeg = (date?: Date) => {
+  const now = date ?? new Date(Date.now());
+  const hour = now.getHours();
+  const minutes = now.getMinutes();
+
+  return hour * 15 + minutes * 0.25;
+};
+
 export const useNowTimeDegree = (config = { updateMillisecond: 1000 }) => {
-  const dateToDeg = (date: Date): number => {
-    const hour = date.getHours();
-    const minutes = date.getMinutes();
-
-    return hour * 15 + minutes * 0.25;
-  };
-
-  const [deg, setDeg] = useState(0);
+  const [nowTimeDegree, setDeg] = useState(calcTimeDeg());
 
   useInterval(() => {
-    setDeg(dateToDeg(new Date(Date.now())));
+    setDeg(calcTimeDeg());
   }, config.updateMillisecond);
 
-  return deg;
+  return nowTimeDegree;
 };
